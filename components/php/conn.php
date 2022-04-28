3e2f51563ad1ea150852f7d1d48c83ebdb16728a
HiagoCS <hiago8cs@gmail.com>
2022-04-28 11:31:42 -0300

    Criado função para inserir perguntas no banco

--- components/php/conn.php
+++ components/php/conn.php
@@ -14,7 +14,16 @@
 		$query = "SELECT * FROM tb_cursos";
 		$result = $GLOBALS['conn']->query($query);
 		while ($cursos = mysqli_fetch_assoc($result)) {
-			echo "<option value='".$cursos['id']."'>".$cursos['nome']."</option>";
+			echo "<option value='".$cursos['id']."'>".$cursos['nome']." - ".$cursos['carga_hr']."</option>";
 		}
 	}
+	function registerQuest($quest, $id_curso){
+		$query = "INSERT INTO tb_perguntas (id, pergunta, id_curso) VALUES (null, '".$quest."', ".$id_curso.")";
+		if($GLOBALS['conn']->query($query)){
+			echo "Inserção concluida!";
+		}
+		else{
+			echo "Erro na inserção";
+		}
+	}	
 ?>
