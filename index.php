8c18486158a6ed20419a048b67ea4cd7f6598fc0
Murilo Gimenez <91744500+murilouwu@users.noreply.github.com>
2022-04-28 09:29:59 -0300

    1.0

---
+++ index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" type="text/css" href="estileMenu.css">
+    <title>Main</title>
+</head>
+<body>
+    <div class="wnavbar p1">
+    	<img src="images\logo.png" class="wlogo">
+    	<nav class="wnav">
+    		<a href="#" class="link">Teste</a>
+    		<a href="#" class="link">visualizar cursos</a>
+    		<a href="#" class="link">Visualizar ETEC</a>
+    		<a href="#" class="link">contato</a>
+    	</nav>
+    </div>
+</body>
+</html>
